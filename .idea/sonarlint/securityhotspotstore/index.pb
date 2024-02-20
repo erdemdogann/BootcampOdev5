7eebf9733d914d55d0db696bfbeabf052d2b72a9
Erdem Doğan <107808469+erdemdogann@users.noreply.github.com>
2024-02-20 23:23:16 +0300

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+k
+;app/src/main/java/com/example/bootcampodev5/MainActivity.kt,b\f\bfe134369a79771b2a2bea450a8fd08018c23cdc
---
+++ app/src/main/java/com/example/bootcampodev5/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.bootcampodev5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bootcampodev5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var islem = ""
+    private var sonuc = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button0.setOnClickListener {
+            islem = "${islem}0"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button1.setOnClickListener {
+            islem = "${islem}1"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button2.setOnClickListener {
+            islem = "${islem}2"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button3.setOnClickListener {
+            islem = "${islem}3"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button4.setOnClickListener {
+            islem = "${islem}4"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button5.setOnClickListener {
+            islem = "${islem}5"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button6.setOnClickListener {
+            islem = "${islem}6"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button7.setOnClickListener {
+            islem = "${islem}7"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button8.setOnClickListener {
+            islem = "${islem}8"
+            binding.textViewSonuc.text = islem
+        }
+        binding.button9.setOnClickListener {
+            islem = "${islem}9"
+            binding.textViewSonuc.text = islem
+        }
+        binding.buttonClear.setOnClickListener {
+            islem = "0"
+            binding.textViewSonuc.text = islem
+        }
+        binding.buttonPlus.setOnClickListener {
+            islem = "${islem}+"
+            binding.textViewSonuc.text = islem
+        }
+        binding.buttonEqual.setOnClickListener {
+            if (islem == ""){
+                binding.textViewSonuc.text = "0"
+            }else {
+                sonuc = islem.split("+").sumOf {
+                    it.toInt()
+                }
+                binding.textViewSonuc.text = "$sonuc"
+                islem = sonuc.toString()
+            }
+        }
+    }
+}
